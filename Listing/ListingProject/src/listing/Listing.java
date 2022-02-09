ec1d737f76059cf48d78621cbfe91e9e689de470
unknown <femerino@millersville.edu>
2022-02-09 01:14:55 +0100

    Implemented logic to check and look for a class
    Implemented logic for the list class method to look for the class
    specified by the user and print its information. Also implemented code
    for list class and list classes methods that make use of the new data structures where classes and attributes
    are stored.

--- Listing/ListingProject/src/listing/Listing.java
+++ Listing/ListingProject/src/listing/Listing.java
@@ -1,5 +1,6 @@
 package listing;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,24 +9,62 @@ public class Listing {
 	
 	//Listing class: Code to be developed
 	
-	public static void listClass(String className) {
+	public static void listClass(ArrayList<Class> list, String className) {
 		
-		List<String> attributesList = Arrays.asList("sup1", "sup2", "sup3");
-		//Check class exits
-		
-		//Get all the info
-		
-		//Print
-		
-
-		System.out.println(" ________________");
-		printRow(className, 0, false);
-		System.out.println("|================|");
-		
-		for(String att: attributesList) {
-			printRow(att, 1, false);
+		if(Driver.validation_check(className)) {
+			
+			Class classAux;
+			
+			for(int i = 0; i < list.size(); i++) {
+				classAux = list.get(i);
+				if(classAux.name.equals(className)) {
+					
+					System.out.println(" ________________");
+					printRow(classAux.name, 0, false);
+					System.out.println("|================|");
+					
+					for(Attribute attr: classAux.attributes) {
+						printRow(attr.name, 1, false);
+					}
+					
+					System.out.println("|________________|");
+				}
+			}
 		}
-		System.out.println("|________________|");
+	}
+	
+	public static void listClasses(ArrayList<Class> list) {
+		for(Class classSample: list) {
+			System.out.println(" ________________");
+			printRow(classSample.name, 0, false);
+			System.out.println("|================|");
+			
+			classSample.attributes.add(new Attribute("Gold"));
+			
+			for(Attribute attr: classSample.attributes) {
+				printRow(attr.name, 1, false);
+			}
+			
+			System.out.println("|________________|");
+		}
+	}
+	
+	public static void listRelationships() {
+		System.out.println(" ________________                              ________________");
+		printRelationship("Car", "Plane");
+		System.out.println("|________________|                            |________________|");
+		
+		System.out.println(" ________________                              ________________");
+		printRelationship("Train", "Boat");
+		System.out.println("|________________|                            |________________|");
+		
+	}
+	
+	public static void printRelationship(String class1, String class2) {
+		printRow(class1, 0, true);
+		System.out.print("     <----Related----->     ");
+		printRow(class2, 0, true);
+		System.out.println("");
 	}
 	
 	private static void printRow(String phrase, int isAttribute, boolean isRelationship) {
@@ -49,40 +88,6 @@ public class Listing {
 			System.out.print("|");
 		else
 			System.out.println("|");
-	}
-	
-	public static void listClasses() {
-		List<String> attributesList = Arrays.asList("sup1", "sup2", "sup3");
-		List<String> classes = Arrays.asList("Car", "Boat", "Plane");
-		
-		for(String className: classes) {
-			System.out.println(" ________________");
-			printRow(className, 0, false);
-			System.out.println("|================|");
-			
-			for(String att: attributesList) {
-				printRow(att, 1, false);
-			}
-			System.out.println("|________________|");
-		}
-	}
-	
-	public static void listRelationships() {
-		System.out.println(" ________________                              ________________");
-		printRelationship("Car", "Plane");
-		System.out.println("|________________|                            |________________|");
-		
-		System.out.println(" ________________                              ________________");
-		printRelationship("Train", "Boat");
-		System.out.println("|________________|                            |________________|");
-		
-	}
-	
-	public static void printRelationship(String class1, String class2) {
-		printRow(class1, 0, true);
-		System.out.print("     <----Related----->     ");
-		printRow(class2, 0, true);
-		System.out.println("");
 	}
 
 }
