4e0cae63112358ac4806ca8346bdbbab15f041b6
cfield167 <70351667+cfield167@users.noreply.github.com>
2022-03-05 02:01:21 -0500

    Prevented indexoutofboundsException

--- app/src/main/java/UML/controller/CLIController.java
+++ app/src/main/java/UML/controller/CLIController.java
@@ -15,28 +15,28 @@ public class CLIController {
         if (tokens[0].equalsIgnoreCase("Create")) {
             // create class
             // Command: create class <class_name>
-            if (tokens[1].equalsIgnoreCase("Class") && lengthChecker(tokens, 3)) {
+            if (lengthChecker(tokens, 3) && tokens[1].equalsIgnoreCase("Class") ) {
                 System.out.println(dg.createClass(tokens[2]));
                 return;
             }
 
             // Create Field
             // Command: create field <class_name> <field_name> <field_type> 
-            if (tokens[1].equalsIgnoreCase("Field") && lengthChecker(tokens, 5)) {
+            if (lengthChecker(tokens, 5) && tokens[1].equalsIgnoreCase("Field")) {
                 System.out.println(dg.createField(tokens[2], tokens[3], tokens[4]));
                 return;
             }
 
             // Create Relationship
             // Command: create relationship <relationship_type> <src> <dest>
-            if (tokens[1].equalsIgnoreCase("Relationship") && lengthChecker(tokens, 5)) {
+            if (lengthChecker(tokens, 5) && tokens[1].equalsIgnoreCase("Relationship")) {
                 System.out.println(dg.createRelationship(tokens[2], tokens[3], tokens[4]));
                 return;
             }
 
             // Create Method
             // Command: create method <class_name> <method_name> <type> <param>
-            if (tokens[1].equalsIgnoreCase("Method") && tokens.length > 4) {
+            if (tokens.length > 4 && tokens[1].equalsIgnoreCase("Method")) {
                 ArrayList <String> params = new ArrayList <String> ();
                 for(int i = 5; i < tokens.length; i++)
                 {
@@ -52,42 +52,42 @@ public class CLIController {
         if (tokens[0].equalsIgnoreCase("Delete")) {
             // delete class
             // Command: delete class <class_name>
-            if (tokens[1].equalsIgnoreCase("Class") && lengthChecker(tokens, 3)) {
+            if (lengthChecker(tokens, 3) && tokens[1].equalsIgnoreCase("Class")) {
                 System.out.println(dg.deleteClass(tokens[2]));
                 return;
             }
 
             // delete field
             // Command: delete field <class_name> <field_name>
-            if (tokens[1].equalsIgnoreCase("Field") && lengthChecker(tokens, 4)) {
+            if (lengthChecker(tokens, 4) && tokens[1].equalsIgnoreCase("Field")) {
                 System.out.println(dg.deleteField(tokens[2], tokens[3]));
                 return;
             }
 
             // delete method
             // Command: delete method <class_name> <method_name>
-            if (tokens[1].equalsIgnoreCase("Method") && lengthChecker(tokens, 4)) {
+            if (lengthChecker(tokens, 4) && tokens[1].equalsIgnoreCase("Method")) {
                 System.out.println(dg.deleteMethod(tokens[2], tokens[3]));
                 return;
             }
 
             // delete relationship
             // Command: delete relationship <relationship_name>
-            if (tokens[1].equalsIgnoreCase("Relationship") && lengthChecker(tokens, 4)) {
+            if (lengthChecker(tokens, 4) && tokens[1].equalsIgnoreCase("Relationship")) {
                 System.out.println(dg.deleteRelationship(tokens[2], tokens[3]));
                 return;
             }
 
             // Delete parameter
             // Command: Delete parameter <class_name> <method_name>  <parameter>
-            if (tokens[1].equalsIgnoreCase("Parameter") && lengthChecker(tokens, 5)) {
+            if (lengthChecker(tokens, 5) && tokens[1].equalsIgnoreCase("Parameter")) {
                 System.out.println(dg.deleteParameter(tokens[2], tokens[3], tokens[4]));
                 return;
             }
 
             // Delete parameters
             // Command: Delete parameters <class_name> <method_name>
-            if (tokens[1].equalsIgnoreCase("Parameters") && lengthChecker(tokens, 4)) {
+            if (lengthChecker(tokens, 4) && tokens[1].equalsIgnoreCase("Parameters")) {
                 System.out.println(dg.deleteParameters(tokens[2], tokens[3]));
                 return;
             }
@@ -96,27 +96,27 @@ public class CLIController {
         if (tokens[0].equalsIgnoreCase("Rename")) {
             // Rename class
             // Command: Rename Class <old_name> <new_name>
-            if (tokens[1].equalsIgnoreCase("Class") && lengthChecker(tokens, 4)) {
+            if (lengthChecker(tokens, 4) && tokens[1].equalsIgnoreCase("Class")) {
                 System.out.println(dg.renameClass(tokens[2], tokens[3]));
                 return;
             }
 
             // Rename field
             // Command: Rename field <class_name> <old_name> <new_name>
-            if (tokens[1].equalsIgnoreCase("field") && lengthChecker(tokens, 5)) {
+            if (lengthChecker(tokens, 5) && tokens[1].equalsIgnoreCase("field")) {
                 System.out.println(dg.renameField(tokens[2], tokens[3], tokens[4]));
                 return;
             }
 
             // Rename method
             // Command: Rename method <class_name> <old_name> <new_name> <type> <param> 
-            if (tokens[1].equalsIgnoreCase("method") && tokens.length > 4) {
+            if (tokens.length > 4 && tokens[1].equalsIgnoreCase("method")) {
                 ArrayList <String> parameter = new ArrayList <String> ();
                 for(int i = 5; i < tokens.length - 5; i += 2)
-                    {
+                {
                     parameter.add(tokens[i]);
                     parameter.add(tokens[i + 1]);
-                    }   
+                }   
                 System.out.println(dg.renameMethod(tokens[2], tokens[3], tokens[4]));
                 return;
             }
@@ -126,14 +126,14 @@ public class CLIController {
         if (tokens[0].equalsIgnoreCase("Change")) {
             // Change single parameter
             // Command: Change parameter <class_name> <method_name> <old_parameter> <new_parameter> <new_parameter_type> 
-            if (tokens[1].equalsIgnoreCase("Parameter") && lengthChecker(tokens, 7)) {
+            if (lengthChecker(tokens, 7) && tokens[1].equalsIgnoreCase("Parameter")) {
                 System.out.println(dg.changeParameter(tokens[2], tokens[3], tokens[4], tokens[5], tokens[6]));
                 return;
             }
 
             // Change list of parameters
             // Command: Change parameters <class_name> <method_name> <param>
-            if (tokens[1].equalsIgnoreCase("Parameters") && tokens.length > 3) {
+            if (tokens.length > 3 && tokens[1].equalsIgnoreCase("Parameters")) {
                 System.out.println(dg.changeParameters(tokens[2], tokens[3], tokens));
                 return;
             }
@@ -145,21 +145,21 @@ public class CLIController {
         if (tokens[0].equalsIgnoreCase("List")) {
             // List class
             // Command: list class <class_name>
-            if (tokens[1].equalsIgnoreCase("Class") && lengthChecker(tokens, 3)) {
+            if (lengthChecker(tokens, 3) && tokens[1].equalsIgnoreCase("Class")) {
                 dg.listClass(tokens[2]);
                 return;
             }
 
             // List classes
             // Command: list classes
-            if (tokens[1].equalsIgnoreCase("Classes") && lengthChecker(tokens, 2)) {
+            if (lengthChecker(tokens, 2) && tokens[1].equalsIgnoreCase("Classes")) {
                 dg.listClasses();
                 return;
             }
 
             // List relationships
             // Command: list relationships
-            if (tokens[1].equalsIgnoreCase("Relationships") && lengthChecker(tokens, 2)) {
+            if (lengthChecker(tokens, 2) && tokens[1].equalsIgnoreCase("Relationships")) {
                 dg.listRelationships();
                 return;
             }
@@ -179,7 +179,7 @@ public class CLIController {
             return;
         }
         // help user
-        if (tokens[0].equalsIgnoreCase("Help")) {
+        if (lengthChecker(tokens, 1) && tokens[0].equalsIgnoreCase("Help")) {
             System.out.printf("%-70s\n\t%-50s\n", "COMMANDS", "USAGE");
 
             System.out.printf("%-70s\n\t%-50s\n", "help", "shows this help message");
@@ -245,11 +245,11 @@ public class CLIController {
     public static boolean lengthChecker(String[] tokens, int size) {
         // Check for valid input length
         if (tokens.length < size) {
-            System.out.println("ERROR: Input contains too few keywords");
+            //System.out.println("ERROR: Input contains too few keywords");
             return false;
         }
         if (tokens.length > size) {
-            System.out.println("ERROR: Input contains too many keywords");
+            //System.out.println("ERROR: Input contains too many keywords");
             return false;
         }
         return true;
