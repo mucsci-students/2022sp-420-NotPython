816dc0ca7e9160dc340a5d3523e7557ceef8385d
unknown <femerino@millersville.edu>
2022-03-31 04:18:09 +0200

    Initial functional version for TabCompletion
    Initial functional version for TabCompletion. TabCompletion works for
    just some defined keywords like 'create', 'delete', 'change'...

--- app/src/main/java/UML/view/CLI.java
+++ app/src/main/java/UML/view/CLI.java
@@ -2,6 +2,14 @@ package UML.view;
 
 import UML.controller.CLIController;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+import java.io.IOException;
+ 
+import jline.TerminalFactory;
+import jline.console.ConsoleReader;
+import jline.console.completer.*;
 
 public class CLI {
     
@@ -29,6 +37,41 @@ public class CLI {
             }
 
             controller.processCommand(tokens);
+        }
+    }
+
+    public void runCLITAB ()
+    {
+        try {
+            StringsCompleter completer = new StringsCompleter("help", "create", "rename", "delete", "change", "save", "load", "list", "exit");
+
+            ConsoleReader console = new ConsoleReader();
+            console.setPrompt(">> ");
+            console.addCompleter(completer);
+            String line = null;
+            String input;
+            String [] tokens = new String [100];
+            CLIController controller = new CLIController();
+            while ((input = console.readLine()) != null) {
+                input = input.strip();
+                tokens = input.split("\\s+");
+
+                //exit the program
+                if(tokens[0].equalsIgnoreCase("exit")) {
+                    break;
+                }
+
+                controller.processCommand(tokens);
+                //console.println(line);
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                TerminalFactory.get().restore();
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
